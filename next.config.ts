1a3e5692b0ee59b3cfaded1ee33d4bcd08c68bd9
dedserch <dedmemchik@gmail.com>
2026-06-20 22:30:38 +0300

    For deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["jawless-sandpit-amused.ngrok-free.dev"],
+  output: "export",
 };
 
 export default nextConfig;
--- widgets/telegram-init-data-panel/ui/telegram-init-data-panel.tsx
+++ widgets/telegram-init-data-panel/ui/telegram-init-data-panel.tsx
@@ -6,12 +6,11 @@ export function TelegramInitDataPanel() {
 	const { telegram } = useTelegramInitData()
 	const username = telegram.user?.username
 		? `@${telegram.user.username}`
-		: telegram.user?.first_name || 'ХУЕСОС'
-
+		: telegram.user?.first_name || 'ЧУВВАААААК'
 	return (
 		<main className='flex min-h-dvh items-center justify-center px-5 text-center'>
 			<h1 className='text-4xl font-semibold tracking-normal sm:text-5xl'>
-				"Здарова!" {username}
+				ЗДАРОВА {username}
 			</h1>
 		</main>
 	)
